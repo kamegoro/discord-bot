36ec8082de00319141482ed5fa987f5f4d72f7d8
kame <tentyo0620@icloud.com>
2022-12-03 11:19:33 +0900

    Add read command files

---
+++ src/register.ts
@@ -0,0 +1,33 @@
+import { REST } from "@discordjs/rest";
+import "dotenv/config";
+import fs from "fs";
+import { Routes } from "discord.js";
+
+const commands = [];
+
+// commandsディレクトリのファイルを全取得
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+// コマンドを配列に突っ込む
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN || "");
+
+// コマンドをdiscordサーバに登録
+(async () => {
+  try {
+    console.log("スラッシュコマンド登録");
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID || "",
+        process.env.GUILD_ID || ""
+      ),
+      { body: commands }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
