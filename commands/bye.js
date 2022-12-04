affc62a76ddd22169eddc65c154ba8ab316dd3bf
kame <tentyo0620@icloud.com>
2022-12-04 17:32:06 +0900

    Add voice library

--- commands/bye.js
+++ commands/bye.js
@@ -10,12 +10,24 @@ module.exports = {
     const memberVC = member.voice.channel;
     if (!memberVC) {
       console.log("接続先のVCが見つかりません。");
+      // return interaction.reply({
+      //     content: "接続先のVCが見つかりません。",
+      //     ephemeral: true,
+      // });
     }
     if (!memberVC.joinable) {
       console.log("VCに接続できません。");
+      // return interaction.reply({
+      //     content: "VCに接続できません。",
+      //     ephemeral: true,
+      // });
     }
     if (!memberVC.speakable) {
       console.log("VCで音声を再生する権限がありません。");
+      // return interaction.reply({
+      //     content: "VCで音声を再生する権限がありません。",
+      //     ephemeral: true,
+      // });
     }
 
     const connection = getVoiceConnection(memberVC.guild.id);
